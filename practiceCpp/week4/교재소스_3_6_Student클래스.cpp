5c529c933a568aec3def6902b239ea88283c4c5c
EunGeunHi_laptop <EunGeunHi@DESKTOP-668KVCQ>
2025-03-25 15:56:16 +0900

    add 4week-1

---
+++ practiceCpp/week4/교재소스_3_6_Student클래스.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+
+class Student {
+private:
+	char* name;
+	int age;
+	char* city;
+
+public:
+	Student(char* name, int age, char* city) {
+		this->name = strdup(name);
+		this->age = age;
+		this->city = strdup(city);
+	}
+
+	~Student() {
+		free(name);
+		free(city);
+	}
+	void show() {
+		std::cout << "Name: " << name << ", Age: " << age << ", City: " << city << std::endl;
+	}
+};
+
+int main() {
+	Student sset2[5] = {
+			{"John", 20, "New York"},
+			{"Alice", 22, "Los Angeles"},
+			{"Bob", 21, "Chicago"},
+			{"Emily", 19, "San Francisco"},
+			{"Michael", 23, "Seattle"}
+	};
+	/*
+	Student sset2[5] = {
+	Student("John", 20, "New York"),
+	Student("Alice", 22, "Los Angeles"),
+	Student("Bob", 21, "Chicago"),
+	Student("Emily", 19, "San Francisco"),
+	Student("Michael", 23, "Seattle")
+};
+	
+	*/
+	for (int i = 0; i < 5; ++i)
+		sset2[i].show();
+
+}
