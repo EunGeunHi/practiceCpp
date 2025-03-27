4c85e18203f64d326a2f7ac96bbdbd71a934703f
EunGeunHi_laptop <EunGeunHi@DESKTOP-668KVCQ>
2025-03-27 15:03:45 +0900

    add 4주차2 파일들

---
+++ practiceCpp/week5/4장_1_static_linkage.cpp
@@ -0,0 +1,14 @@
+//4Àå_static_linkage.cpp
+int g = 2017;//static duration, external linkage
+static int s = 20;//static duration, internal linkage
+const int c = 30;//const global º¯¼ö
+void show();
+int main() {
+	show();
+	return 0;
+}
+void show() {
+	static int n = 0;//static duration, no linkage
+	//...
+		return 0;
+}
