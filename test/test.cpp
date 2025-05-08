46783c0dbf036d63f27b9595c1638f2902880de0
EunGeunHi_laptop <EunGeunHi@DESKTOP-668KVCQ>
2025-05-08 16:10:55 +0900

    4.10

--- test/test.cpp
+++ test/test.cpp
@@ -5,124 +5,82 @@
 #include <cstring>
 
 using namespace std;
+const int MAX_SIZE = 100;
 
-class Car {
+
+class Vehicle{
 private:
-	char* manufacturer;
-	//string manufacturer; 버전도 연습
-	int speed;
-
-	static int count;
-
-	char* dupStr(const char* str) {	//메모리 할당후 복사
-		char* ret = new char[strlen(str) + 1];
-		strcpy(ret, str);
-		return ret;
-	}
-
+    char* manufacturer;
+    char* model;
+    int price;
+protected:
+    static char* copyString(const char* src);
 public:
-	//[생성자]
-	//디폴트
-	Car() {
-		manufacturer = dupStr("Unknown");
-		speed = 0;
-		++count;
-		cout << "default constructor 호출" << endl;
-	}
-	//파라미터
-	Car(const char* manufacturer, int speed) : 
-		speed(speed) {
-		this->manufacturer = dupStr(manufacturer);
-		++count;
-		cout << "paramiterized constructor 호출" << endl;
-	}
-	//복사
-	Car(Car& other) {
-		manufacturer = dupStr(other.manufacturer);
-		speed = other.speed;
-		++count;
-		cout << "copy constructor 호출" << endl;
-	}
-	// 소멸자
-	~Car() {
-		delete[] manufacturer;
-		--count;
-		cout << "destructor 호출" << endl;
-	}
-
-	//[Getter Setter]
-	const char* getManufacturer() const { return manufacturer; }
-	const int getSpeed()const { return speed; }
-	static const int getCarCount() { return count; }
-	/*함수 정의 뒤에 있는 const는 const 객체에서도 사용할수 있다는 뜻*/
-	Car& setManufacturer(const char* str) {
-		delete[] manufacturer;
-		manufacturer = dupStr(str);
-		return *this;
-	}
-	Car& setSpeed(int speed) {
-		if (isValidSpeed(speed)) {
-			this->speed = speed;
-		}
-		else {
-			cout << "유효하지 않은 속도 입니다. (0~300 km/h)\n";
-			this->speed = 0;  // 기본값
-		}
-		return *this;
-	}
-private:
-	bool isValidSpeed(int speed) const { //속도가 유효한 값인지 판별
-		return speed >= 0 && speed <= 300;
-	}
-
-public:
-
-	void showCars() const {
-		cout << "제조사: " << manufacturer
-			<< ", 속도: " << speed << " km/h"
-			<< ", 현재 Car 객체 수: " << count << endl;
-	}
-	void compareSpeed(const Car& other) const {
-		if (this->speed > other.speed)
-			cout << manufacturer << "가 " << other.getManufacturer() << "보다 빠릅니다.\n";
-		else if (this->speed < other.speed)
-			cout << manufacturer << "가 " << other.getManufacturer() << "보다 느립니다.\n";
-		else
-			cout << manufacturer << "와 " << other.getManufacturer() << "의 속도가 같습니다.\n";
-	}
-
-	//대입 연산자
-	Car& operator=(const Car& other) {
-		if (this == &other) return *this;
-
-		delete[] manufacturer;
-		manufacturer = dupStr(other.manufacturer);
-		speed = other.speed;
-		
-		cout << "대입 연산자 호출됨" << endl;
-	}
+    Vehicle(const char* m = "", const char* md = "", int p = 0);
+    Vehicle(const Vehicle& other);
+    virtual~Vehicle();
+    virtual void show() const;
 };
-
-int Car::count = 0; //static 멤버변수
-
+class Car: virtual public Vehicle{
+private:
+    int passengers;
+    double engineSize;
+public:
+    Car(const char* m, const char* md, int p, int people, double e)
+        : Vehicle(m, md, p), passengers(people), engineSize(e) {
+    }
+    void show() const override;
+};
+class Truck:virtual public Vehicle {};
+class CarTruck: public Car,public Truck{};
+class Battery{};
+class ElectricCar: public Vehicle, public Battery{};
+class HybridCar: public Car, public Battery{};
+class UsedVehicleStore {
+private:
+    Vehicle** table;
+    int count;
+public:
+    UsedVehicleStore(Vehicle** t = new Vehicle * [MAX_SIZE], int c = 0);
+    ~UsedVehicleStore();
+    void add(Vehicle* v);
+    void show() const;
+};
 int main() {
-	const int SIZE = 5;
+    UsedVehicleStore store;
 
-	Car* table1 = new Car[SIZE];
-	table1[0].setManufacturer("현대").setSpeed(100);
-	cout << "table1[1] 수정" << endl;
-	table1[1] = Car("토요타", 120);
-	cout << "table1[2] 잘못된 수정" << endl;
-	table1[2].setManufacturer("기아").setSpeed(-50);
+    store.add(new Car("Hyundai", "Avante", 1800, 5, 1.6));
+    store.add(new Truck("Kia", "Bongo", 2300, 1.5));
+    store.add(new CarTruck("Hyundai", "Porter 2", 2500, 3, 2.0, 1.2));
+    store.add(new ElectricCar("Tesla", "Model 3", 5500, 75));
+    store.add(new HybridCar("Toyota", "Prius", 3200, 4, 1.8, 45));
 
-	cout << "\n=== carSet[i].showCars() ===" << endl;
-	for (int i = 0; i < SIZE; ++i) {
-		table1[i].showCars();
-	}
+    store.show();  // 오버로딩된 show 사용
 
-	cout << "\n현재 전체 Car 객체 수:: " << Car::getCarCount() << endl;
-
-
-
-
+    int choice;
+    cin >> choice;
+    return 0;
 }
+
+
+UsedVehicleStore::UsedVehicleStore(Vehicle** t=new Vehicle*[MAX_SIZE], int c=0): count(c){
+        table = new Vehicle * [c];
+        for (int i = 0; i < c; ++i) 
+            table[i] = t[i];    //shallow copy, delete at main()
+    }
+UsedVehicleStore::~UsedVehicleStore(){
+        delete[] table;  //shallow copy, delete at main()
+    }
+void UsedVehicleStore:: add( Vehicle* v) {
+        if (count < MAX_SIZE) {
+            table[count] = v;
+            ++count;
+        }
+    }
+void UsedVehicleStore:: show() const{
+        for (int i = 0; i < count; ++i) {
+            table[i]->show();
+            cout << endl;
+        }
+    }
+};
